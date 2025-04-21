f73932d29b860b23c85b4d25a926249bb9bb6f3f
keeplearningpro <108283264+keeplearningpro@users.noreply.github.com>
2025-04-21 19:17:45 +1000

    Added function to read excel in iframe

--- security.js
+++ security.js
@@ -1,3 +1,15 @@
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+function showIframe() {
+    const iframeContainer = document.getElementById('iframeContainer');
+    const iframe = document.getElementById('surveyFrame');
+
+    Using Google Docs Viewer to read excel in frame
+    const fileUrl = "https://keeplearningpro.github.io/fintech-crypto-research/data/survey.xlsx";
+    const gviewUrl = "https://docs.google.com/gview?embedded=true&url=" + encodeURIComponent(fileUrl);
+
+    iframe.src = gviewUrl;
+    iframeContainer.style.display = 'block';
+  }
