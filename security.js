dca5fe47eb0dc9f5e208d148db9994058a39af75
keeplearningpro <108283264+keeplearningpro@users.noreply.github.com>
2025-04-21 21:03:53 +1000

    Added function to generate Pie Chart for Familiarity with cryptocurrency

--- security.js
+++ security.js
@@ -14,3 +14,47 @@ function toggleIframe() {
       button.textContent = "View Survey Results";
     }
   }
+
+//Function for Pie Chatt for Familiarity With Cryptocurrency
+window.addEventListener('DOMContentLoaded', function () {
+  const ctx = document.getElementById('cryptoFamiliarityChart').getContext('2d');
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: [
+        "Cryptocurrency is the future of finance.",
+        "I am concerned there are too many scams",
+        "I avoid them because I don’t trust the system",
+        "I don’t have any investment at the moment but will use them.",
+        "I don’t use them because they are for techy people.",
+        "Yes. I have already invested in them."
+      ],
+      datasets: [{
+        label: 'Familiarity with Cryptocurrency',
+        data: [15, 15, 20, 15, 12, 23],
+        backgroundColor: [
+          "#6baed6",
+          "#de2d26",
+          "#74c476",
+          "#9e9ac8",
+          "#a1d99b",
+          "#fd8d3c"
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'right'
+        },
+        title: {
+          display: true,
+          text: 'Familiarity with Cryptocurrency'
+        }
+      }
+    }
+  });
+});
