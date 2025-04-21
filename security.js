5a36fc7aa63df0be910e3fd20e3e481873a9654a
keeplearningpro <108283264+keeplearningpro@users.noreply.github.com>
2025-04-21 19:22:25 +1000

    Update security.js

--- security.js
+++ security.js
@@ -6,7 +6,7 @@ function showIframe() {
     const iframeContainer = document.getElementById('iframeContainer');
     const iframe = document.getElementById('surveyFrame');
 
-    Using Google Docs Viewer to read excel in frame
+    //Using Google Docs Viewer to read excel in frame
     const fileUrl = "https://keeplearningpro.github.io/fintech-crypto-research/data/survey.xlsx";
     const gviewUrl = "https://docs.google.com/gview?embedded=true&url=" + encodeURIComponent(fileUrl);
 
