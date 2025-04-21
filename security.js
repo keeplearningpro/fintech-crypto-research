94d698caab05ff52d1ec6e5b02f41435409ec33f
keeplearningpro <108283264+keeplearningpro@users.noreply.github.com>
2025-04-21 19:38:59 +1000

    Update security.js

--- security.js
+++ security.js
@@ -3,13 +3,5 @@ function scrollToTop() {
 }
 
 function showIframe() {
-    const iframeContainer = document.getElementById('iframeContainer');
-    const iframe = document.getElementById('surveyFrame');
-
-    //Using Google Docs Viewer to read excel in frame
-    const fileUrl = "https://keeplearningpro.github.io/fintech-crypto-research/data/survey.xlsx";
-    const gviewUrl = "https://docs.google.com/gview?embedded=true&url=" + encodeURIComponent(fileUrl);
-
-    iframe.src = gviewUrl;
-    iframeContainer.style.display = 'block';
+  document.getElementById('iframeContainer').style.display = 'block';
   }
