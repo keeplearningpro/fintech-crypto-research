7b8c44223e98a40d4d0d5090d6ce52ca17d749cf
keeplearningpro <108283264+keeplearningpro@users.noreply.github.com>
2025-04-22 17:23:19 +1000

    adding function to show / hide blocks

--- security.js
+++ security.js
@@ -3,6 +3,13 @@ function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
+//Function to show hide blocks
+function showBlock(id, btn) {
+      const block = document.getElementById(id);
+      block.classList.remove('hidden');
+      btn.style.display = 'none';
+    }
+
 //Function to show/hide for iframe
 function toggleIframe() {
     const iframeContainer = document.getElementById("iframeContainer");
