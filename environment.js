9547706d19c849aba4e98957988007df3accba35
keeplearningpro <108283264+keeplearningpro@users.noreply.github.com>
2025-04-25 21:27:58 +1000

    Update environment.js

--- environment.js
+++ environment.js
@@ -1,3 +1,10 @@
     function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
+
+//Function to show hide blocks
+function showBlock(id, btn) {
+      const block = document.getElementById(id);
+      block.classList.remove('hidden');
+      btn.style.display = 'none';
+    }
